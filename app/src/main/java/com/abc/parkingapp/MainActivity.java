2de54cbbde174db98c91422890bc2771c09866b5
Ivmens <65495212+Ivmens@users.noreply.github.com>
2021-01-05 10:28:48 +0200

    v0.2 Databese connection WIP

--- app/src/main/java/com/abc/parkingapp/MainActivity.java
+++ app/src/main/java/com/abc/parkingapp/MainActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +19,36 @@ public class MainActivity extends AppCompatActivity {
     private EditText Password;
     private Button Register;
     private Button Login;
+    private static final String TAG = "my-activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            Log.d(TAG, "nu ir galīgā dirsā");
+            e.printStackTrace();}
+
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://databases-auth.000webhost.com/id15829689_carparking", "id15829689_admin", "*Superdrosaparole221*")) {
+
+            if (conn != null) {
+                Log.d(TAG, "ir conn");
+            } else {
+                Log.d(TAG, "nav conn");
+            }
+
+        } catch (SQLException e) {
+            Log.d(TAG, "nu ir dirsā");
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+            e.printStackTrace();
+            Log.d(TAG, "nu ir dirsā");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         Email = (EditText)findViewById(R.id.etEmail);
         Password = (EditText)findViewById(R.id.etPassword);
